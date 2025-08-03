a9b5f63897f4f677efb16736e25ca51eec73f22c
Bassam <Bassam.darawshi@gmail.com>
2025-08-03 11:26:37 +0300

    argon

--- app/core/security.py
+++ app/core/security.py
@@ -7,8 +7,7 @@ from app.core.config import get_settings
 
 settings = get_settings()
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
+pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
 SECRET_KEY = settings.SECRET_KEY
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = getattr(settings, "ACCESS_TOKEN_EXPIRE_MINUTES", 60000)
